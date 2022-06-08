8341a22da35a6d9bf51b0a7b2f2d83e57287f736
Alan Pedraza <alanbanano@gmail.com>
2022-06-08 17:13:59 -0500

    Almost Final Implementation

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import json
+
+def write_json(new_data, filename='data.json'):
+    with open(filename,'r+') as file:
+          # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["RegisteredPeople"].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file)
 
 app = FastAPI()
 
@@ -13,5 +25,27 @@ templates = Jinja2Templates(directory="templates")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {
         "request": request,
-        "message": "Hola gente, vamos a usar html con FastAPI"
     })
+
+@app.get("/dashboard", response_class=HTMLResponse)
+async def dashboard(request: Request):
+
+    f = open('data.json')
+
+    data = json.load(f)
+
+    print(data)
+
+    return templates.TemplateResponse("info.html", {
+        "request": request,
+        "data": data
+    })
+
+@app.post("/addUsers")
+async def addUsers(users: Request):
+    data = await users.json()
+
+    for user in data:
+        write_json(user)
+
+    return { "user_added": "Usuario Agregado" }
